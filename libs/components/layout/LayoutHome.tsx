446c4cd386acb6ab66cbd2a70de055aa633bd3a8
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-08-03 07:22:25 +0900

    layout

---
+++ libs/components/layout/LayoutHome.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import useDeviceDetect from '../../hooks/useDeviceDetect';
+import Head from 'next/head';
+import Top from '../Top';
+import Footer from '../Footer';
+import { Stack } from '@mui/material';
+import FiberContainer from '../common/FiberContainer';
+import HeaderFilter from '../homepage/HeaderFilter';
+import { userVar } from '../../../apollo/store';
+import { useReactiveVar } from '@apollo/client';
+import { getJwtToken, updateUserInfo } from '../../auth';
+import Chat from '../Chat';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+const withLayoutMain = (Component: any) => {
+	return (props: any) => {
+		const device = useDeviceDetect();
+		const user = useReactiveVar(userVar);
+
+		/** LIFECYCLES **/
+		useEffect(() => {
+			const jwt = getJwtToken();
+			if (jwt) updateUserInfo(jwt);
+		}, []);
+
+		/** HANDLERS **/
+
+		if (device == 'mobile') {
+			return (
+				<>
+					<Head>
+						<title>ta-Go</title>
+						<meta name={'title'} content={`ta-Go`} />
+					</Head>
+					<Stack id="mobile-wrap">
+						<Stack id={'top'}>
+							<Top />
+						</Stack>
+
+						<Stack id={'main'}>
+							<Component {...props} />
+						</Stack>
+
+						<Stack id={'footer'}>
+							<Footer />
+						</Stack>
+					</Stack>
+				</>
+			);
+		} else {
+			return (
+				<>
+ 
+				</>
+			);
+		}
+	};
+};
+
+export default withLayoutMain;
